ddd8110a8bdfe1ec0abe2b1f1acec453df399554
Abdul Ajoge <bugsabdul@yahoo.com>
2020-09-10 14:34:25 +0100

    add name to customer schema

--- models/customer.js
+++ models/customer.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose');
 
 const customerSchema = new mongoose.Schema({
-    firstname: {
+    name: {
         type: String,
         required: true
-    },
-
-    lastname: {
-        type: String,
-        required: true
-    },
+    }
 
     email: {
         type: String,
